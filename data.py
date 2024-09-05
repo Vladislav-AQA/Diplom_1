5dc95dfc2eceb3993cb947bc94e4bfbfb75f2476
Vladislav Stepanov <vladislavforeverstepanov@yandex.ru>
2024-09-05 21:56:15 +0300

    Commit of Diplom_1

---
+++ data.py
@@ -0,0 +1,13 @@
+from unittest import Mock
+
+mock_bun = Mock()
+mock_bun.get_name.return_value = 'Булка'
+mock_bun.get_price.return_value = 100
+
+mock_filling = Mock()
+mock_filling.get_name.return_value = 'Котлета'
+mock_filling.get_price.return_value = 20
+
+mock_sauce = Mock()
+mock_sauce.get_name.return_value = 'Кетчуп'
+mock_sauce.get_price.return_value = 1
